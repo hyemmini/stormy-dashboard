5b56d7f0bd1a22645daa9361e1c3d6bea8be5e6b
hyemin <hmjjang93@gmail.com>
2025-07-15 16:41:36 +0900

    Rollback BaseRouterName

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/stormy-dashboard/', //github pages
+  base: '/', //github pages
   plugins: [react()],
 });
