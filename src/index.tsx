1080da4d13a2be00bc62782ab477e089a76412ce
hyemin <hmjjang93@gmail.com>
2025-07-15 16:02:36 +0900

    Fixed BrowserRouter basename

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/stormy-dashboard">
         <App />
       </BrowserRouter>
     </QueryClientProvider>
